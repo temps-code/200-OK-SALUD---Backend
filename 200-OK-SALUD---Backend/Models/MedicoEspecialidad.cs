00b89ffb3f8c2e8146f045e855512f7b9a4580a9
temps-code <diego.v.u21@gmail.com>
2025-05-31 19:59:12 -0400

    feat: diseño base del la estructura de las tablas, integrando identity para auth

---
+++ 200-OK-SALUD---Backend/Models/MedicoEspecialidad.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _200_OK_SALUD___Backend.Models
+{
+    public class MedicoEspecialidad
+    {
+        [Key, Column(Order = 0)]
+        [ForeignKey(nameof(Medico))]
+        public int MedicoId { get; set; }
+        public Medico Medico { get; set; }
+
+        [Key, Column(Order = 1)]
+        [ForeignKey(nameof(Especialidad))]
+        public int EspecialidadId { get; set; }
+        public Especialidad Especialidad { get; set; }
+    }
+}
---
+++ 200-OK-SALUD---Backend/Models/Paciente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _200_OK_SALUD___Backend.Models
+{
+    public class Paciente
+    {
+        [Key]
+        public int PacienteId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Usuario))]
+        public string UsuarioId { get; set; }
+        public ApplicationUser Usuario { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Apellido { get; set; }
+
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+
+        [MaxLength(1)]
+        public string Genero { get; set; }  // 'M', 'F' u 'O'
+
+        [MaxLength(20)]
+        public string Telefono { get; set; }
+
+        // ESTA LÍNEA es la que faltaba: colección de navegación a Cita
+        public ICollection<Cita> Citas { get; set; }
+    }
+}
